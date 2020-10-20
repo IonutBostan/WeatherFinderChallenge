e925b410c7fe146f7fef48d218bddef41ec5b006
Eric Ros <ericros@dorna.com>
2020-10-20 19:44:39 +0200

    refactor: react hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import getWeather from "../actions/getWeather";
+import getWeatherAction from "../actions/getWeather";
 import AppTitle from "./AppTitle";
 import WeatherForm from "./WeatherForm";
 import WeatherInfo from "./WeatherInfo";
 
-export default class App extends React.Component {
-  state = {
+const App = () => {
+  const [weather, setWeather] = useState({
     temperature: undefined,
     city: undefined,
     country: undefined,
     humidity: undefined,
     description: undefined,
     error: undefined
-  };
-  getWeather = async ev => {
+  });
+  const getWeather = async ev => {
     ev.preventDefault();
     const elements = ev.target.elements;
-    const weather = await getWeather(elements);
-    this.setState({ ...weather });
+    const weather = await getWeatherAction(elements);
+    setWeather({ ...weather });
   };
-  render() {
-    return (
-      <div>
-        <div className="wrapper">
-          <div className="main">
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-5 title-container">
-                  <div>
-                    <AppTitle />
-                  </div>
+
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="main">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-5 title-container">
+                <div>
+                  <AppTitle />
                 </div>
-                <div className="col-7 form-container">
-                  <WeatherForm onSubmit={this.getWeather} />
-                  <WeatherInfo info={this.state} />
-                </div>
+              </div>
+              <div className="col-7 form-container">
+                <WeatherForm onSubmit={getWeather} />
+                <WeatherInfo info={weather} />
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
